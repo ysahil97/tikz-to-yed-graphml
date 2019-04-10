4b9946507b6b3e841666f50741a53c4ebb759d46
proton0001 <cs15btech11029@iith.ac.in>
2019-04-10 20:53:49 +0530

    Added code for Draw and Foreach

--- parserForTikz.py
+++ parserForTikz.py
@@ -1,6 +1,104 @@
 import sys
 import re
 import os
+import copy
+from pprint import pprint
+
+def handleNode(line):
+    NodeID = re.findall("\((\w+)\)", line)
+    Location = re.findall("at[\s]*\((.*?)\)", line)
+    NodeProperties = re.findall("\[.*?\]", line)
+    Label = re.findall("{.*?}", line)
+    if NodeID:
+        NodeID = NodeID[0]
+    else:
+        NodeID = None
+    if(Label):
+        Label = Label[0].strip('{').strip('}')
+    else:
+        Label = None
+    if(Location):
+        coordinates = []
+        for coord in Location[0].split(','):
+            coordinates.append(float(coord))
+        Location = coordinates
+    else:
+        Location = None
+    if(NodeProperties):
+        properties = {}
+        for prop in NodeProperties[0].split(','):
+            l = prop.split('=')
+            properties[l[0]] = l[1]
+        NodeProperties = properties
+    else:
+        NodeProperties = None
+    # print "-------------------------------------NODESTART"
+    # print line
+    # print NodeID, 
+    # print Label,
+    # print Location, 
+    # print NodeProperties
+    # print "-------------------------------------NODEEND"
+
+
+
+def handleDraw(line):
+    a = re.findall("\\\\draw[\s]*\((.*?)\)(.*?)\((.*?)\)", line)
+    A = a[0][0].split(",")
+    B = a[0][1].strip()
+    if(B== "ellipse"):
+        C = a[0][2].split("and")
+        C = [x.strip().strip("cm") for x in C]
+    else:
+        C = a[0][2].split(",")
+    print A, B, C
+    
+
+
+def handleForeach(block):
+    # print block
+
+    line = block[0]
+    remainBlock='\n'.join(block[1:])
+    toReturn=[]
+    if(line.__contains__("\\foreach")):
+        searchIterators = re.findall("\\\\foreach(.*)in.*", line)
+        if(searchIterators):
+            searchIterators = searchIterators[0]
+        
+        loopIter = searchIterators.strip().split("/")
+        variables = {}
+        var_n_to_name = {}
+        i = 0
+        for var in loopIter:
+            variables[var.strip("\\")] = None
+            var_n_to_name[i] = var.strip("\\")
+
+            i+=1
+        
+        domain = re.findall("{(.*)}", line)
+
+        if(domain):
+            domain = domain[0]
+        # print(variables , domain)
+        for a in domain.split(","):
+            newBlock = copy.copy(remainBlock)
+            for n, b in enumerate(a.split("/")):
+                variables[var_n_to_name[n]] = b
+                newBlock = re.sub("\\\\"+var_n_to_name[n], b, newBlock)
+        
+            toReturn.append(copy.copy(newBlock))
+    # print "-------------------------------------FOREACH_START"
+    # pprint (block)
+    # pprint (toReturn)
+    # print "-------------------------------------FOREACH_END"
+
+
+
+
+
+
+
 
 def parseTiKZ(inputFile):
     """
@@ -22,43 +120,43 @@ def parseTiKZ(inputFile):
     globalSpecs=z.groups()[0].strip()
     mainCode=z.groups()[1].strip()
     insideForEach=False
+    maybeInsideForEach=False
+    block = []
     for line in mainCode.split('\n'):
         line=line.strip()
+        brackets = 0
+        if(line.__contains__("\\foreach")):
+            x=re.findall("\\\\foreach.*in[\s]*{.*}(.*)", line)
+            y=re.findall("\\\\foreach.*in[\s]*{.*?}.*", line)
+            if(x):
+                x = x[0]
+            if(y):
+                y = y[0]
+            block.append(y)
+            block.append(x)
+            brackets = line.count("{") - line.count("}")
+            insideForEach=True   
+
+        elif(insideForEach):
+            block.append(line)
+            if(line[0] == "{"):
+                brackets = brackets + line.count("{") - line.count("}")                
+                if(brackets==0):
+                    insideForEach=False
+                    handleForeach(block)
+                    # print "CASE_A_:", block
+                    block = []
+            else:
+                insideForEach=False
+                handleForeach(block)
+                # print "CASE_B_:", block
+                block = []
+            continue
+                
         if(line.__contains__("\\node")):
-            NodeID = re.findall("\((\w+)\)", line)
-            Location = re.findall("at[\s]*\((.*?)\)", line)
-            NodeProperties = re.findall("\[.*?\]", line)
-            Label = re.findall("{.*?}", line)
-            if NodeID:
-                NodeID = NodeID[0]
-            else:
-                NodeID = None
-            if(Label):
-                Label = Label[0].strip('{').strip('}').strip("$")
-            else:
-                Label = None
-            if(Location):
-                coordinates = []
-                for coord in Location[0].split(','):
-                    coordinates.append(float(coord))
-                Location = coordinates
-            else:
-                Location = None
-            if(NodeProperties):
-                properties = {}
-                for prop in NodeProperties[0].split(','):
-                    l = prop.split('=')
-                    properties[l[0]] = l[1]
-                NodeProperties = properties
-            else:
-                NodeProperties = None
-
-            print NodeID, 
-            print Label,
-            print Location, 
-            print NodeProperties
-
-        # if(insideForEach):
+           handleNode(line)
+        if(line.__contains__("\\draw")):
+            handleDraw(line)
 
 
             
@@ -87,7 +185,7 @@ def parseTiKZ(inputFile):
 
 
 # fileName = "rg-v2.tex"
-fileName="edge-editing-v2.tex"
+fileName="TestCases/edge-editing-v2.tex"
 with open(fileName) as inputFile:
     fileContent = inputFile.read()
 regextToGetTikzPictureCode = re.compile("\\\\end[\s]*{tikzpicture}", re.MULTILINE)
