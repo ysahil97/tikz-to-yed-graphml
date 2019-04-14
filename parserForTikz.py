7e13e4f0acc4953996bf41fe7a7990ab1aa04ac9
proton0001 <cs15btech11029@iith.ac.in>
2019-04-14 18:52:12 +0530

    Removed unnecessay code in parseTikz.py file

--- parserForTikz.py
+++ parserForTikz.py
@@ -4,7 +4,7 @@ import os
 import copy
 from pprint import pprint
 
-def handleNode(line):
+def handleNode(line, globalProperties_2):
     NodeID = re.findall("\((\w+)\)", line)
     Location = re.findall("at[\s]*\((.*?)\)", line)
     NodeProperties = re.findall("\[.*?\]", line)
@@ -37,15 +37,21 @@ def handleNode(line):
     print NodeID, 
     print Label,
     print Location, 
-    print NodeProperties
+    
+    # TODO: NodeProperties may override some globalProperties
+    print NodeProperties, 
+    if(globalProperties_2.has_key("node")):
+        print (globalProperties_2["node"])
+    else:
+        print ""
     # print "-------------------------------------NODEEND"
 
 
 
+# TODO : Handle Polar Coordinates
 def handleDraw(line):
-    # print "TODO: ", line
-    pass
-    # TODO : Handle Polar Coordinates
+    print "TODO: ", line
+    # pass
     # a = re.findall("\\\\draw[\s]*\((.*?)\)(.*?)\((.*?)\)", line)
     # if(a):
     #     A = a[0][0].split(",")
@@ -57,70 +63,11 @@ def handleDraw(line):
     #         C = a[0][2].split(",")
     #     print A, B, C
     # else:
-    #     print "TODO : ", line
+    #     print "todo : ", line
     #     a = re.findall("\\\\draw[\s]*\((.*?)\)(.*?)\((.*?)\)", line)
         
     
-
-
-def handleForeach(block):
-    # print block
-    # sys.exit(1)
-
-    line = block[0]
-    if(len(block) > 1 and block[1].strip()=="{"):
-        remainBlock='\n'.join(block[2:])
-    else:
-        remainBlock='\n'.join(block[1:])
-                
-    # print remainBlock, block
-    toReturn=[]
-    if(line.__contains__("\\foreach")):
-        searchIterators = re.findall("\\\\foreach(.*)in.*", line)
-        if(searchIterators):
-            searchIterators = searchIterators[0]
-        
-        loopIter = searchIterators.strip().split("/")
-        variables = {}
-        var_n_to_name = {}
-        i = 0
-        for var in loopIter:
-            variables[var.strip("\\")] = None
-            var_n_to_name[i] = var.strip("\\")
-
-            i+=1
-        
-        domain = re.findall("{(.*)}", line)
-
-        if(domain):
-            domain = domain[0]
-        # print(variables , domain)
-        for a in domain.split(","):
-            newBlock = copy.copy(remainBlock)
-            for n, b in enumerate(a.split("/")):
-                variables[var_n_to_name[n]] = b
-                newBlock = re.sub("\\\\"+var_n_to_name[n], b, newBlock)
-            toReturn.append(copy.copy(newBlock).strip())
-    # print toReturn
-    # sys.exit(1)
-    for line in toReturn:
-        # print "----------->" , line
-        if(line.__contains__("\\node")):
-           handleNode(line)
-        if(line.__contains__("\\draw")):
-            handleDraw(line)
-
-
-
-
-
-
-
-
 def parseTiKZ(inputFile):
-    print "INPUT FILE STARTS --------------------------------"
-    print inputFile
-    print "INPUT FILE ENDS --------------------------------"
     fileRegex = re.compile("(\s*\\[[^\\\\]*\\])((.|\n)*)", re.MULTILINE)
     z = fileRegex.match(inputFile)
     globalSpecs=z.groups()[0].strip()
@@ -167,77 +114,26 @@ def parseTiKZ(inputFile):
     block = []
     for line in mainCode.split(';'):
         line=line.strip()
-        brackets = 0
-        if(line.__contains__("\\foreach")):
-            x=re.findall("\\\\foreach.*in[\s]*{.*}(.*)", line)
-            y=re.findall("\\\\foreach.*in[\s]*{.*?}.*", line)
-            if(x):
-                x = x[0]
-            if(y):
-                y = y[0]
-            block.append(y)
-            block.append(x)
-            brackets = line.count("{") - line.count("}")
-            insideForEach=True   
-
-        elif(insideForEach):
-            block.append(line)
-            # print "-------->", line
-            brackets = brackets + line.count("{") - line.count("}")                
-            # if(line[0] == "{"):
-            if(brackets==0):
-                insideForEach=False
-                handleForeach(block)
-                # print "CASE_A_:", block
-                block = []
-            # else:
-                
-            #     insideForEach=False
-            #     handleForeach(block)
-            #     # print "CASE_B_:", block
-            #     block = []
-            # continue
-                
-        if(line.__contains__("\\node")):
-           handleNode(line)
         if(line.__contains__("\\draw")):
             handleDraw(line)
+        if(line.__contains__("\\node")):
+            handleNode(line, globalProperties_2)
 
 
-            
-
-    #         pass
-    #     else:
-    #         if(line.__contains__("foreach")):
-    #             insideForEach=True
-    #     print (line.strip())
-    # if(line.__contains__("\\foreach")):
-    #     if(line.__contains__("{")):
-    #         foreach_brackets_opened=True
-
-        
-    # elif(line.__contains__("\\draw"))
-    # elif(line.__contains__("\\node"))
-    # elif(line.__contains__("foreach"))
-    # elif(line.__contains__("foreach"))
-    # elif(line.__contains__("foreach"))
-    # sys.exit(1)
 
 if __name__ == "__main__":
     # fileName = "TestCases/rg-v2.tex"
-    fileName="TestCases/edge-editing-v2.tex"
-    with open(fileName) as inputFile:
-        fileContent = inputFile.read()
-    regextToGetTikzPictureCode = re.compile("\\\\end[\s]*{tikzpicture}", re.MULTILINE)
-
-    tikzBlocks = []
-    for x in regextToGetTikzPictureCode.split(fileContent):
-        if(x.__contains__("tikzpicture")):
-            tikzBlocks.append(x)
-    count = -1
-    for x in tikzBlocks:
-        for y in (re.split("\\\\begin{tikzpicture}", x)):
-            count += 1
-            if(count%2==1):
-                parseTiKZ(y)
-                # sys.exit(1)
+    fileName_prefix="/home/pankaj/acads/Sem8/softEng/project/tikz-to-yed-graphml/edge-editing-v2.tex"
+    fileName_suffix="_unrolled.tex"
+    j=0
+    while(os.path.exists(fileName_prefix + "t" + str(j) + fileName_suffix)):
+        fileName = fileName_prefix + "t" + str(j) + fileName_suffix
+        with open(fileName) as inputFile:
+            fileContent = inputFile.read()
+        print ("======================================")
+        print ("Parsing File :", fileName)
+        print (fileContent)
+        print ("======================================")
+        parseTiKZ(fileContent.strip())
+        j+=1
+    
