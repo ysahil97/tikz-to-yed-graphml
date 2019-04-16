ad20875a9e4458099003b1aaf93a9f317e4f5c95
proton0001 <cs15btech11029@iith.ac.in>
2019-04-16 11:02:46 +0530

    Fixes #1

--- parserForTikz.py
+++ parserForTikz.py
@@ -4,27 +4,38 @@ import os
 import copy
 from pprint import pprint
 
-def handleNode(line, globalProperties_2):
+
+
+def handleNode(line, globalProperties):
+    #  \node (nodeidentifier) at (Location: polar or cartession) {Label};
+     # This regex will capture the alphanumeric text inside parenthesis since Location involves ',' and ':' which are not alphanumeric, it won't be captures 
     NodeID = re.findall("\((\w+)\)", line)
+    # Location is always in parenthesis after at .. so search for "at ()" and capture the text inside parenthesis
     Location = re.findall("at[\s]*\((.*?)\)", line)
+    # everything inside [] is nodeproperties
     NodeProperties = re.findall("\[.*?\]", line)
+    # Text inside {} contains node label
     Label = re.findall("{.*}", line)
+
     if NodeID:
         NodeID = NodeID[0]
     else:
         NodeID = None
-    if(Label):
+
+    if Label:
         Label = Label[0]
     else:
         Label = None
-    if(Location):
+    
+    if Location:
         coordinates = []
         for coord in Location[0].split(','):
             coordinates.append(coord)
         Location = coordinates
     else:
         Location = None
-    if(NodeProperties):
+    
+    if NodeProperties:
         properties = {}
         for prop in NodeProperties[0].split(','):
             l = prop.split('=')
@@ -32,23 +43,38 @@ def handleNode(line, globalProperties_2):
         NodeProperties = properties
     else:
         NodeProperties = None
+
     # print "-------------------------------------NODESTART"
     # print line
-    print NodeID, 
+    print NodeID,
     print Label,
-    print Location, 
+    print Location,
+    globalNodeProp = {}
+
+    # if(globalProperties and globalProperties.has_key("node")):
+    #     globalNodeProp = globalProperties["node"]
+    # else:
+    #     for key, val in NodeProperties.iteritems():
+    #         print key, val
+
+    # if(NodeProperties):
+    #     for key, val in globalNodeProp.iteritems():
+    #         if(NodeProperties.has_key(key)):
+    #             print key, NodeProperties[key]
+    #         else:
+    #             print key, val
+    # else:
+    #     for key, val in globalNodeProp.iteritems():
+    #         print key, val
+
+    print(NodeProperties, globalProperties)
     
-    # TODO: NodeProperties may override some globalProperties
-    print NodeProperties, 
-    if(globalProperties_2.has_key("node")):
-        print (globalProperties_2["node"])
-    else:
-        print ""
     # print "-------------------------------------NODEEND"
 
 
 
 # TODO : Handle Polar Coordinates
+# TODO : Handle all draw cases
 def handleDraw(line):
     print "TODO: ", line
     # pass
@@ -66,31 +92,43 @@ def handleDraw(line):
     #     print "todo : ", line
     #     a = re.findall("\\\\draw[\s]*\((.*?)\)(.*?)\((.*?)\)", line)
         
-    
+
+# Get globalSpecs and Code from inputFile
+# Go through mainCode and handle each instruction (Only node for now)
 def parseTiKZ(inputFile):
+    # Tikz Code format is :
+    # [GlobalProperties] MainCode
+    # This regex captures GlobalProperties and MainCode as group 0 and 1 respectively
     fileRegex = re.compile("(\s*\\[[^\\\\]*\\])((.|\n)*)", re.MULTILINE)
     z = fileRegex.match(inputFile)
-    globalSpecs=z.groups()[0].strip()
-    if(globalSpecs):
-        globalSpecs = globalSpecs.strip("[").strip("]")
+    if(len(z.groups())!=3):
+        print ("Cannot Parse file")
+        return
+    matchedGlobalSpecs=z.groups()[0]
+    if(matchedGlobalSpecs):
+        matchedGlobalSpecs = matchedGlobalSpecs.strip().strip("[").strip("]")
     brakets = 0
-    joinedProp = []
-    globalProperties = []
-    for l in globalSpecs.split(','):
+    subProp = []
+    allProps = []
+    # Normal split won't work in cases like below
+    # scale=.8,auto=center,every node/.style={circle,inner sep=4pt}
+    # Because of "every node/.style={circle,inner sep=4pt}"
+    for l in matchedGlobalSpecs.split(','):
         if(brakets  > 0):
             brakets += l.count("{")-l.count("}")
-            joinedProp.append(l)
+            subProp.append(l)
             if(brakets==0):
-                globalProperties.append(",".join(joinedProp))
+                allProps.append(",".join(subProp))
         else:
             brakets += l.count("{")-l.count("}")
             if(brakets>0):
-                joinedProp.append(l)
+                subProp.append(l)
             else:
-                globalProperties.append(l) 
-    globalProperties_2 = {}
-    for x in globalProperties:
-        key, val =  x.split('=', 1)
+                allProps.append(l) 
+
+    globalProperties = {}
+    for prop in globalProperties:
+        key, val =  prop.split('=', 1)
         if(key.__contains__("node")):
             val = val.strip("{").strip("}")
             temp = {}
@@ -102,24 +140,22 @@ def parseTiKZ(inputFile):
                 else:
                     temp["shape"] = temp_2[0]
 
-                globalProperties_2["node"] = temp
+                globalProperties["node"] = temp
         elif(key.__contains__("edge")):
-            globalProperties_2["edge"] = val
+            # TODO: Not seen this so not handled (added just in case)
+            globalProperties["edge"] = val
         else:
-            globalProperties_2[key] = val
-    print globalProperties_2
-    mainCode=z.groups()[1].strip()
-    insideForEach=False
-    maybeInsideForEach=False
-    block = []
-    for line in mainCode.split(';'):
+            globalProperties[key] = val
+
+    print globalProperties
+
+    tikzCode=z.groups()[1].strip()
+    for line in tikzCode.split(';'):
         line=line.strip()
         if(line.__contains__("\\draw")):
             handleDraw(line)
         if(line.__contains__("\\node")):
-            handleNode(line, globalProperties_2)
-
-
+            handleNode(line, globalProperties)
 
 if __name__ == "__main__":
     # fileName = "TestCases/rg-v2.tex"
@@ -132,8 +168,7 @@ if __name__ == "__main__":
             fileContent = inputFile.read()
         print ("======================================")
         print ("Parsing File :", fileName)
-        print (fileContent)
+        # print (fileContent)
         print ("======================================")
         parseTiKZ(fileContent.strip())
         j+=1
-    
