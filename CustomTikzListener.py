375206caa566481854830bf10c7d8fad9c97410e
sipian <cs15btech11019@iith.ac.in>
2019-04-23 00:00:57 +0530

    generate pyyed nodes with coordinates, labels

--- CustomTikzListener.py
+++ CustomTikzListener.py
@@ -1,23 +1,49 @@
 import sys
+import logging
 from antlr4 import *
 from grammar.TikzListener import TikzListener
 from grammar.TikzParser import TikzParser
+from generateGraphml import Graph
 
 class CustomTikzListener(TikzListener) :
-    def __init__(self):
+    def __init__(self, inputFileName:str, outputFileName:str):
         self.nodes = []
-    
+        self.G = Graph()
+        self.inputFileName = inputFileName
+        self.outputFileName = outputFileName
+
+    def exitBegin(self, ctx:TikzParser.BeginContext):
+        try:
+            logging.info("Trying to convert Tikz into GraphML")
+            graphml = self.G.get_graph().encode("utf-8")
+            with open(self.outputFileName, 'wb') as outFile:
+                outFile.write(graphml)
+            logging.info("Converted Tikz Graph to GraphML.\n\n\t\tGraphML File Location: {}\n\n".format(self.outputFileName))
+        except Warning as e:
+            logging.warn("Error in converting {} - {}".format(self.inputFileName, e))
+
     def exitNode(self, ctx:TikzParser.NodeContext):
-        print("NodeID: ", ctx.nodeID().text)
-        print("coordinates: ", ctx.coordinates().x, ctx.coordinates().y)
-        print("label: ", ctx.label())
+        self.G.addNode(
+            ctx.nodeID().id,
+            ctx.coordinates().x,
+            ctx.coordinates().y,
+            label=ctx.label().label
+        )
 
     def exitNodeID(self, ctx:TikzParser.NodeIDContext):
-        id = ctx.ID().getText()
-        ctx.text = id
+        if ctx.ID() is not None:
+            ctx.id = ctx.ID().getText()
+        else:
+            ctx.id = None
 
     def exitCoordinates(self, ctx:TikzParser.CoordinatesContext):
         x = ctx.DIGIT(0).getText()
         y = ctx.DIGIT(1).getText()
         ctx.x = x
         ctx.y = y
+
+    def exitLabel(self, ctx:TikzParser.LabelContext):
+        if ctx.ID() is not None:
+            ctx.label = ctx.ID().getText()
+        else:
+            ctx.label = ""
--- parseTikz.py
+++ parseTikz.py
@@ -1,19 +1,26 @@
 import sys
 import antlr4
+import logging
 from grammar.TikzLexer import TikzLexer
 from grammar.TikzListener import TikzListener
 from grammar.TikzParser import TikzParser
 from CustomTikzListener import CustomTikzListener
 
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-1s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
 def main():
-    fileName = './TestCases/graph.tex'
-    input_stream = antlr4.FileStream(fileName)
+    inputFileName = './TestCases/graph.tex'
+    input_stream = antlr4.FileStream(inputFileName)
     lexer = TikzLexer(input_stream)
     stream = antlr4.CommonTokenStream(lexer)
     parser = TikzParser(stream)
     tree = parser.begin()
 
-    htmlChat = CustomTikzListener()
+    htmlChat = CustomTikzListener(inputFileName, './TestCases/graph.graphml')
     walker = antlr4.ParseTreeWalker()
     walker.walk(htmlChat, tree)
 
