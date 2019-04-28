83aa3b183cad67c2f25dc605c1897de9829c7347
sipian <cs15btech11019@iith.ac.in>
2019-04-29 01:21:30 +0530

    Add error handling of parser error

--- parseTikz.py
+++ parseTikz.py
@@ -1,13 +1,14 @@
+import os
 import sys
 import antlr4
 import logging
-import os
+import argparse
 from grammar.TikzLexer import TikzLexer
 from grammar.TikzParser import TikzParser
 from grammar.TikzListener import TikzListener
+from TikzErrorListener import TikzErrorListener
 from CustomTikzListener import CustomTikzListener
 from extradeCodeInsideTikzAndUnrollForeach import getCodeInsideTIKZAfterUnrolling
-import argparse
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-1s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
@@ -15,19 +16,24 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-1s [%(filename)s:
 
 logger = logging.getLogger(__name__)
 
+def printContents(value):
+    for i, line in enumerate(value.split('\n')):
+        print(i+1, ": ", line)
+
 def main(scalingFactor, logLevel, inputFilename, prefix, directory):
     
     if not prefix:
         prefix = inputFilename
 
     for value in getCodeInsideTIKZAfterUnrolling(directory, inputFilename):
-        logger.info("===================================")
-        logger.info("\n\n" + value + "\n\n")
-        logger.info("===================================")
+        logger.info("\n\n===================================\n\n")
+        printContents(value)
+        logger.info("\n\n===================================\n\n")
         input_stream = antlr4.InputStream(value)
         lexer = TikzLexer(input_stream)
         stream = antlr4.CommonTokenStream(lexer)
         parser = TikzParser(stream)
+        parser.addErrorListener(TikzErrorListener())
         tree = parser.begin()
 
         # we save file as filename_t_{n}_graph.graphml
