cc7074323c11d14d70040a3f99e1d203f1cb8eff
ysahil97 <cs15btech11044@iith.ac.in>
2019-04-29 04:11:17 +0530

    nits

--- parseTikz.py
+++ parseTikz.py
@@ -16,53 +16,32 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-1s [%(filename)s:
 
 logger = logging.getLogger(__name__)
 
-def printContents(value):
-    for i, line in enumerate(value.split('\n')):
-        print(i+1, ": ", line)
+class ParseTikz:
+    def printContents(self,value):
+        for i, line in enumerate(value.split('\n')):
+            print(i+1, ": ", line)
 
-def main(scalingFactor, logLevel, inputFilename, prefix, directory):
+    def run(self,scalingFactor, logLevel, inputFilename, prefix, directory):
 
-    if not prefix:
-        prefix = inputFilename
+        if not prefix:
+            prefix = inputFilename
 
-    for value in getCodeInsideTIKZAfterUnrolling(inputFilename):
-        logger.info("\n\n===================================\n\n")
-        printContents(value)
-        logger.info("\n\n===================================\n\n")
-        input_stream = antlr4.InputStream(value)
-        lexer = TikzLexer(input_stream)
-        stream = antlr4.CommonTokenStream(lexer)
-        parser = TikzParser(stream)
-        parser.addErrorListener(TikzErrorListener())
-        tree = parser.begin()
+        for value in getCodeInsideTIKZAfterUnrolling(inputFilename):
+            logger.info("\n\n===================================\n\n")
+            self.printContents(value)
+            logger.info("\n\n===================================\n\n")
+            input_stream = antlr4.InputStream(value)
+            lexer = TikzLexer(input_stream)
+            stream = antlr4.CommonTokenStream(lexer)
+            parser = TikzParser(stream)
+            parser.addErrorListener(TikzErrorListener())
+            tree = parser.begin()
 
-        # we save file as filename_t_{n}_graph.graphml
-        j = 0
-        while(os.path.exists(directory +"/" + prefix + "_" + str(j) + "_graph.graphml")):
-            j += 1
-        outputFilename = directory +"/" + prefix + "_" + str(j) + "_graph.graphml"
-        htmlChat = CustomTikzListener(inputFilename, outputFilename, scalingFactor)
-        walker = antlr4.ParseTreeWalker()
-        walker.walk(htmlChat, tree)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("-h", "--help", type=int, help="Print this menu")
-    parser.add_argument("-v", "--verbosity", type=int, help="increase output verbosity", default=0)
-    parser.add_argument("-s", "--scale", type=float, help="Scaling Factor", default=200)
-    parser.add_argument("-i", "--input", type=str, help="Input file path")
-    parser.add_argument("-p", "--prefix", type=str, help="Output file Prefix")
-    parser.add_argument("-d", "--directory", type=str, help="Output file directory")
-
-    args = parser.parse_args()
-
-    scalingFactor = args.scale
-    logLevel = args.scale
-    inputFileName = args.input
-    prefix = args.prefix
-    directory = "./"
-    if args.directory:
-        directory=args.directory
-
-    main(scalingFactor, logLevel, inputFileName, prefix, directory)
-
+            # we save file as filename_t_{n}_graph.graphml
+            j = 0
+            while(os.path.exists(directory +"/" + prefix + "_" + str(j) + "_graph.graphml")):
+                j += 1
+            outputFilename = directory +"/" + prefix + "_" + str(j) + "_graph.graphml"
+            htmlChat = CustomTikzListener(inputFilename, outputFilename, scalingFactor)
+            walker = antlr4.ParseTreeWalker()
+            walker.walk(htmlChat, tree)
