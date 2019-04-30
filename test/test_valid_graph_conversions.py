27af25c2e4acff110858a497f61df5255bad9f22
ysahil97 <cs15btech11044@iith.ac.in>
2019-05-01 00:28:04 +0530

    Basic code for unit-testing

--- test/test_valid_graph_conversions.py
+++ test/test_valid_graph_conversions.py
@@ -1,20 +1,26 @@
-import unittest 
+import unittest
+from parseTikz import ParseTikz
 
 x = [
     {
-        "inputFilePath" : "./TestCases/valid1.tex",
-        "expectedOutputPath" : 
-        "generatedOutputPath" : 
+        "inputFilePath" : "./TestCases/label.tex",
+        "expectedOutputPath" : "./TestCases/label.tex_0_graph.graphml"
+        "generatedOutputPath" : "./TestCases/label.tex_legacy_0_graph.graphml"
     }
-] 
+]
 
-class TestValidGraphConversions(unittest.TestCase): 
+class TestValidGraphConversions(unittest.TestCase):
+    def __init__():
+        self.parseModule = ParseTikz()
 
-    def test_split(self):         
+    def test_split(self):
+        for i in x:
+            inputFilePath = i["inputFilePath"]
+            
         s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world']) 
-        with self.assertRaises(TypeError): 
-            s.split(2) 
-  
-if __name__ == '__main__': 
-    unittest.main() 
+        self.assertEqual(s.split(), ['hello', 'world'])
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
